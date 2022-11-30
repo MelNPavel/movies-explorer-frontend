2a16fd89e9ba5fc6dad9b073f9995920b55609bb
Pavel Melnikov <PavelNMel@yandex.ru>
2022-11-30 23:22:37 +0300

    fix: На проверку после 1 ревью

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,8 +1,8 @@
 const myJwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmY0MTUyMTYzOTBhNDAwMTQ2OTYxMDkiLCJpYXQiOjE2NjAxNjM1MDN9.nMr1EsDiPO1LmmC1oksqDJ3elyXwpbkbU7NEKQBeuH4";
 
 const configApiMain = {
-  baseUrl: '//localhost:4000',
-  // baseUrl: 'https://api.movies-explorer-mpn.nomoredomains.icu',
+  // baseUrl: '//localhost:4000',
+  baseUrl: 'https://api.movies-explorer-mpn.nomoredomains.icu',
   headers: {    
     'Content-Type': 'application/json'
   },
@@ -14,6 +14,8 @@ const configApiMovies = {
     'Content-Type': 'application/json'
   },
 };
+
+
 
 export {
     myJwt,
